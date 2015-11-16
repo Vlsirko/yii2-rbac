5a7de1f8dcd8c5716a3028a52f6bd041b9e6c868
Влад <vlad.sirenko@bodo.ua>
2015-11-16 13:22:45 +0200

    add module

---
+++ Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Umcms\modules\UserPermitions;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'Umcms\modules\UserPermitions\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
