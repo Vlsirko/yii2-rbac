54b525bc28f5428c2daf43fa689a7860dc7361bf
Vlsirko <Vlsirko@i.ua>
2015-12-03 11:05:15 +0200

    Update m151120_104918_create_rbac_permissions_list.php

--- migrations/m151120_104918_create_rbac_permissions_list.php
+++ migrations/m151120_104918_create_rbac_permissions_list.php
@@ -7,23 +7,23 @@ class m151120_104918_create_rbac_permissions_list extends Migration
 {
     public function up()
     {
-		$createPermittion = \yii::$app->authManager->createPermission('rbac\roles\create');
+		$createPermittion = \yii::$app->authManager->createPermission('rbac/roles/create');
 		$createPermittion->description = 'Создание групп пользователей';
 		\yii::$app->authManager->add($createPermittion);
 		
-		$indexPermittion = \yii::$app->authManager->createPermission('rbac\roles\index');
+		$indexPermittion = \yii::$app->authManager->createPermission('rbac/roles/index');
 		$indexPermittion->description = 'Просмотр листинга групп пользователей';
 		\yii::$app->authManager->add($indexPermittion);
 		
-		$viewPermittion = \yii::$app->authManager->createPermission('rbac\roles\view');
+		$viewPermittion = \yii::$app->authManager->createPermission('rbac/roles/view');
 		$viewPermittion->description = 'Просмотр группы пользователей';
 		\yii::$app->authManager->add($viewPermittion);
 		
-		$deletePermittion = \yii::$app->authManager->createPermission('rbac\roles\delete');
+		$deletePermittion = \yii::$app->authManager->createPermission('rbac/roles/delete');
 		$deletePermittion->description = 'Удаление группы пользователей';
 		\yii::$app->authManager->add($deletePermittion);
 		
-		$updatePermittion = \yii::$app->authManager->createPermission('rbac\roles\update');
+		$updatePermittion = \yii::$app->authManager->createPermission('rbac/roles/update');
 		$updatePermittion->description = 'Редактирование группы пользователей';
 		\yii::$app->authManager->add($updatePermittion);
 		
